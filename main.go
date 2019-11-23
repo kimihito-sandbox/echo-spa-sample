bee06b356568342ce494f1442ddb8a56600c5e0c
kimihito <tatsurotamashiro@gmail.com>
2019-11-23 13:56:42 +0900

    Embeded resource

--- main.go
+++ main.go
@@ -2,16 +2,24 @@ package main
 
 import (
 	"net/http"
+	"os"
 	"github.com/labstack/echo"
-	"github.com/labstack/echo/middleware"
+	rice "github.com/GeertJohan/go.rice"
 )
 
 func main() {
 	e := echo.New()
-	e.Use(middleware.StaticWithConfig(middleware.StaticConfig{
-		Root: "web/build",
-		HTML5: true,
-	}))
+	env := os.Getenv("ENV")
+
+	if env == "production" {
+		assetHandler := http.FileServer(rice.MustFindBox("web/build").HTTPBox())
+
+		e.GET("/", echo.WrapHandler(assetHandler))
+
+		e.GET("/static/*", echo.WrapHandler(assetHandler))
+
+	}
+
 	e.GET("/api", func(c echo.Context) error {
 		return c.String(http.StatusOK, "Hello, World!")
 	})
